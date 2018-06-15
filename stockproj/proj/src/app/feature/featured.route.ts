f294addcb8d73e8512733e9d22ea6c8f6bad1391
dip <diphalder555@gmail.com>
2018-06-15 14:54:10 +0800

    image import

--- stockproj/proj/src/app/feature/featured.route.ts
+++ stockproj/proj/src/app/feature/featured.route.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { InvestmentAnalyticsComponent } from './investment-analytics/investment-analytics.component';
 import { PageNotFoundComponent } from '../core/page-not-found/page-not-found.component';
+import {bodyComponent} from '../body/body/body.component';
 
  const routes: Routes = [
-    { path: '',   redirectTo: '/mojo', pathMatch: 'full'},
-    { path: 'mojo', component: MainMenuComponent},
+    { path: '',   redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: bodyComponent},
+    { path: 'app', component: MainMenuComponent},
     { path: 'investment',   component: InvestmentAnalyticsComponent },
     { path: '**', component: PageNotFoundComponent }
   ];
