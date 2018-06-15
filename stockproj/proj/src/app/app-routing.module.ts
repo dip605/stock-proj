c65804602968d0c003490ef4882ea01f4c76233f
Nilesh Sinha <nileshinit@gmail.com>
2018-06-15 11:34:09 +0530

    added nav-bar

--- stockproj/proj/src/app/app-routing.module.ts
+++ stockproj/proj/src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 import { bodyComponent } from './body/body/body.component';
-import { InvestmentAnalyticsComponent } from './feature/investment-analytics/investment-analytics.component';
 import { AppComponent } from './app.component';
 const appRoutes: Routes = [
-  { path: '', component: bodyComponent },
-  { path: 'app',   redirectTo: '/MOJO', pathMatch: 'full'},
-  { path: 'MOJO/investment',   component: InvestmentAnalyticsComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: 'app', loadChildren: './feature/feature.module#FeatureModule' }
 ];
 
 @NgModule({
--- stockproj/proj/src/app/feature/featured.route.ts
+++ stockproj/proj/src/app/feature/featured.route.ts
@@ -2,8 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { NgModule } from '@angular/core';
 
+import { InvestmentAnalyticsComponent } from './investment-analytics/investment-analytics.component';
+import { PageNotFoundComponent } from '../core/page-not-found/page-not-found.component';
+
  const routes: Routes = [
-    { path: 'MOJO', component: MainMenuComponent,}
+    { path: '',   redirectTo: '/mojo', pathMatch: 'full'},
+    { path: 'mojo', component: MainMenuComponent},
+    { path: 'investment',   component: InvestmentAnalyticsComponent },
+    { path: '**', component: PageNotFoundComponent }
   ];
 
   @NgModule({
